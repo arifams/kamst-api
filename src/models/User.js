b97a463f3b292405421ed548711d04382b0b8ec1
arifams <arif.amsterdam@gmail.com>
2018-01-10 16:31:08 +0100

    adding verivication email and parsing errors

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,31 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import uniqueValidator from 'mongoose-unique-validator';
 
 // doing some validation on API
 const schema = new mongoose.Schema(
   {
-  email: { type: String, required: true, lowercase: true, index: true },
-  passwordHash: { type: String, required: true }
-  }, 
-  { timeStamps: true }
+  email: { 
+    type: String, 
+    required: true, 
+    lowercase: true, 
+    index: true, 
+    unique: true 
+  },
+  passwordHash: { type: String, required: true },
+  confirmed: { type: Boolean, default: false },
+  },
+  { timestamps: true }
 );
 
 schema.methods.isValidPassword = function isValidPassword(password) {
   return bcrypt.compareSync(password, this.passwordHash);
+};
+
+// Sync the password hash between react app and api
+schema.methods.setPassword = function setPassword(password) {
+  this.passwordHash = bcrypt.hashSync(password, 10);
 };
 
 // use jwt with sign method to create and encrypt the user web token
@@ -31,8 +44,11 @@ schema.methods.generateJWT = function generateJWT() {
 schema.methods.toAuthJSON = function toAuthJSON() {
   return {
   	email: this.email,
+    confirmed: this.confirmed,
   	token: this.generateJWT()
   }
 };
+
+schema.plugin(uniqueValidator, { message: "This email is already registered" });
 
 export default mongoose.model('User', schema);
